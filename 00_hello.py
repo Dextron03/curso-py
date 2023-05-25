4dd4e6a02f084cd818d59388bd0c71d1351b2077
BrailyRS <brailyrs03@gmail.com>
2023-05-24 23:28:36 -0400

    Estamos en la leccion de las variables, por ahora nos a ensañado a declarar una variable, a dar un valor a la variable y como saber tu tipo

--- 00_hello.py
+++ 00_hello.py
@@ -7,6 +7,7 @@ print("Hola python")
 #Type: consulta el tipo de dato:
 print(type("Hola python")) #el resultado que nos daria es un string (str)
 print(type(25)) #el resultado que nos daria es un entero o integer (int)
+print(type(5+5j)) #el resultad que nos daria es un complex 
 print(type(1.5)) #el resultado que nos daria es un float  (float)
 print(type(True)) #el resultado que nos daria es un tipo booleano
 
---
+++ 01_variables.py
@@ -0,0 +1,22 @@
+#Variables
+#forma correcta, camel case
+myvariable = "My string variable"
+print (myvariable)
+
+#forma correcta, snake case
+my_int_variable = 25
+print (my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+
+my_bool_variable = True
+print (my_bool_variable)
+
+print (f"{myvariable}, {my_int_variable}, {my_bool_variable} ")
+
+#Forma erronea, no podemos empezar a escribir el nombre de una variable con mayuscula
+Myvariable = "My string variable"
+print (Myvariable)

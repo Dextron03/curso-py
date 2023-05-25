1c8eceaae53326fa0c2f10f04c4393ad1e0f6f2a
BrailyRS <brailyrs03@gmail.com>
2023-05-24 23:02:08 -0400

    Primera leccion completada, en esta nos da un pequeño repaso sobre los tipos de datos que hay tales como str,int,bool, etc, tambien nos enseña hacer 2 tipos de comentario el de una linea y el de multi-linea, y para finalizar nos enseña a imprimir datos como el famoso 'Hola python'

---
+++ 00_hello.py
@@ -0,0 +1,17 @@
+# Esto es un comendario:
+# Primer hola mundo en python
+print("Hola python")
+
+ 
+
+#Type: consulta el tipo de dato:
+print(type("Hola python")) #el resultado que nos daria es un string (str)
+print(type(25)) #el resultado que nos daria es un entero o integer (int)
+print(type(1.5)) #el resultado que nos daria es un float  (float)
+print(type(True)) #el resultado que nos daria es un tipo booleano
+
+"""
+  Este es un
+  comentario en
+  varias linea
+"""
